a05fff7db7cec5e26ef1ffb6e7eee15f60065022
fyang5-commits <fyang5@inside.artcenter.edu>
2025-11-04 15:03:04 -0800

    Update sketch.js

--- week7/scripts/sketch.js
+++ week7/scripts/sketch.js
@@ -9,11 +9,12 @@ function draw() {
   
   for(let x= 0; x <width; x +=50) {
     for(let y = 0; y < height; y +=50) {
+        let size = map(noise(x * 0.01, y * 0.01, frameCount * 0.01), 0, 1, 10, 40)
+      fill ( x, y, 200);
       rect (x, y, 30, 30)
-      circle (x, y, 20)
+      circle (x, y, size);
     }
   }
-  
   fill (0, 255, 220)
   circle (150, 150, 220)
   
